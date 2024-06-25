e2280f47f43e3d8762676584d692fa83c27b0bd5
Your Name <your.email@example.com>
2024-06-25 17:26:10 +0300

    configurations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("from ayham");
+import { as } from "./varaiables/cardNewVariables";
+
+console.log("asd");
+// console.log("cardNewVariables.currentTemperature", cardNewVariables?.currentTemperature);
+console.log(as);
